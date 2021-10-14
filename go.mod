fdd07e58eed9d6c6982b6173c82954e296b23f8e
lx <zhangluxin1995@gmail.com>
2021-10-15 00:13:31 +0800

    初始化项目目录

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/LuXinZ/bookstore_users_api
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import ("github.com/LuXinZ/bookstore_users_api/app")
+
+func main()  {
+	app.StartApplication()
+}
